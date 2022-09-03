401a5a3a72e37141557287d099d5369e677e5816
Vansarr <ajendrulek@gmail.com>
2022-09-03 20:55:24 +0200

    5.0

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello';
+  const apiRoot = 'https://kodilla-tasks-aj.herokuapp.com/v1/tasks';
+  const trelloApiRoot = 'https://trello.com/thevansar';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
